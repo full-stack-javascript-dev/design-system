19a863d00b41321ecd6f68ded52dff44694783cd
Chris Webb <kungpochicken@gmail.com>
2019-11-24 21:57:01 +1100

    updates

--- src/exports.ts
+++ src/exports.ts
@@ -1,4 +1,8 @@
-import { NavBar } from "./components/NavBar/NavBar";
+import { NavBar,
+  NavBarLogo,
+  NavBarMenu,
+  NavBarMenuItem,
+  NavBarMenuItemComponent } from "./components/NavBar/NavBar";
 import { Button } from "./components/Button";
 import { ButtonRound } from "./components/ButtonRound";
 import { ButtonText } from "./components/ButtonText";
@@ -6,7 +10,7 @@ import { CanvasBlur } from "./components/CanvasBlur";
 import { CarouselPromo } from "./components/Carousel";
 import { CheckBox } from "./components/Checkbox";
 import { Content } from "./components/Content";
-import { ContentList } from "./components/ContentList";
+import { ContentList, ContentListItem } from "./components/ContentList";
 import {
   ControlledTransitionAnimation,
   ControlledKeyframeAnimation
@@ -18,15 +22,20 @@ import { H1, H2, H3, H4 } from "./components/Headings";
 import { ImgResponsive } from "./components/ImgResponsive";
 import { Input } from "./components/Input";
 import { ModalDialog } from "./components/ModalDialog";
-import { PageInfoBar } from "./components/PageInfoBar";
+
+import { PageInfoBar, PageInfoBarContent } from "./components/PageInfoBar";
 import { Root } from "./components/Root";
 import { Table } from "./components/Table";
-import { TagList } from "./components/TagList";
+import { TagList, TagComponent } from "./components/TagList";
 import { Text } from "./components/Text";
 import { ToggleButton } from "./components/ToggleButton";
 
-import { PageFooter } from "./components-layout/PageFooter/PageFooter";
-import { PageHeader } from "./components-layout/PageHeader/PageHeader";
+import { PageFooter, PageFooterIcon } from "./components-layout/PageFooter/PageFooter";
+import {
+  PageHeader,
+  PageHeaderContent
+} from "./components-layout/PageHeader/PageHeader";
+import { PageHeaderWrapper } from "./components-layout/PageHeader/PageHeaderWrapper";
 import {
   LayoutContentColumn,
   LayoutContentColumns
@@ -35,6 +44,10 @@ import { ResponsiveContainer } from "./components-layout/ResponsiveContainer";
 
 export {
   NavBar,
+  NavBarLogo,
+  NavBarMenu,
+  NavBarMenuItem,
+  NavBarMenuItemComponent,
   Button,
   ButtonRound,
   ButtonText,
@@ -43,6 +56,7 @@ export {
   CheckBox,
   Content,
   ContentList,
+  ContentListItem,
   ControlledTransitionAnimation,
   ControlledKeyframeAnimation,
   DropDownMenu,
@@ -58,13 +72,18 @@ export {
   Input,
   ModalDialog,
   PageInfoBar,
+  PageInfoBarContent,
   Root,
   Table,
   TagList,
+  TagComponent,
   Text,
   ToggleButton,
   PageFooter,
+  PageFooterIcon,
   PageHeader,
+  PageHeaderWrapper,
+  PageHeaderContent,
   LayoutContentColumn,
   LayoutContentColumns,
   ResponsiveContainer
